0b4a09cb79ef5457f73f6f0584c4198a3d7d217a
YevheniiaMushyk <142007023+YevheniiaMushyk@users.noreply.github.com>
2024-01-15 16:54:14 +0000

    Deploying to gh-pages from @ YevheniiaMushyk/goit-js-hw-11@1d47b5821a084f829439119746da28cc95c5e6c6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,28 +1,28 @@
-import{i as c,S as u}from"./assets/vendor-46aac873.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const o of s.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&a(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();const l=document.querySelector(".data-select"),n=document.querySelector(".gallery-list");l.addEventListener("submit",i=>{i.preventDefault();const t=l.elements.request.value.trim();f(t).then(r=>d(r)).catch(r=>{c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"})}),l.reset()});const m=new URLSearchParams({image_type:"photo",orientation:"horizontal",safesearch:!0});function f(i){return fetch(`https://pixabay.com/api/?key=41825347-2a0e6255edbe790f7737a6334&q=${i}&${m}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}function d(i){if(n.children.length>0&&n.children.forEach(t=>{t.remove()}),i.totalHits>0){const t=i.hits.map(r=>`<li class="gallery-item">
-          <a class="gallery-link" href="${r.largeImageURL}">
+import{i as c,S as u}from"./assets/vendor-46aac873.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const l of r.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const n=document.querySelector(".data-select"),a=document.querySelector(".gallery-list");n.addEventListener("submit",i=>{i.preventDefault();const t=n.elements.request.value.trim();f(t).then(o=>d(o)).catch(o=>{c.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight"})}),n.reset()});const m=new URLSearchParams({image_type:"photo",orientation:"horizontal",safesearch:!0});function f(i){return fetch(`https://pixabay.com/api/?key=41825347-2a0e6255edbe790f7737a6334&q=${i}&${m}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()})}function d(i){const t=i.hits.map(s=>`<li class="gallery-item">
+          <a class="gallery-link" href="${s.largeImageURL}">
     	      <img
 		          class="gallery-image"
-		          src="${r.webformatURL}"
-		          alt="${r.tags}"
+		          src="${s.webformatURL}"
+		          alt="${s.tags}"
     	      />
             <ul class="info-list">
               <li class="info-item">
                 <h class="item-title">Likes</h>
-                <p class="item-content">${r.likes}</p>
+                <p class="item-content">${s.likes}</p>
               </li>
               <li class="info-item">
                 <h class="item-title">Views</h>
-                <p class="item-content">${r.views}</p>
+                <p class="item-content">${s.views}</p>
               </li>
               <li class="info-item">
                 <h class="item-title">Comments</h>
-                <p class="item-content">${r.comments}</p>
+                <p class="item-content">${s.comments}</p>
               </li>
               <li class="info-item">
                 <h class="item-title">Downloads</h>
-                <p class="item-content">${r.downloads}</p>
+                <p class="item-content">${s.downloads}</p>
               </li>
             </ul>
   		    </a>
-        </li>`).join("");n.insertAdjacentHTML("beforeend",t)}new u(".gallery-list a",{captionsData:"alt",captionDelay:250})}
+        </li>`).join("");a.innerHTML="",a.insertAdjacentHTML("beforeend",t),new u(".gallery-list a",{captionsData:"alt",captionDelay:250}).refresh()}
 //# sourceMappingURL=commonHelpers.js.map
